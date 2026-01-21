fb859ab77846768e19088ca331a6a21ccbd00cff
vishnuwebdev2425 <akkupallivishnu2424@gmail.com>
2026-01-21 16:35:03 +0530

    2 nd Problem related to Product

--- Practise.java
+++ Practise.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Vishnu {
     public static void main(String[] args){
         int [] arr={3,1,2,4,5};
@@ -20,6 +22,41 @@ class Vishnu {
             }
         }
 
+
+    }
+}
+
+
+class Problem2{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter the Length of Array");
+        int a =scan.nextInt();
+        System.out.println();
+        int  [] intial=new int [a];
+        int [] finalarray =new int [a];
+        for (int i=0;i<a;i++){
+            System.out.print("Enter the Value : ");
+            int value=scan.nextInt();
+            intial[i]=value;
+            
+        }
+        for(int i=0;i<a;i++){
+            int prod=1;
+            for(int j=0;j<a;j++){
+                if(i==j){
+                    continue;
+                }else{
+                    prod=prod*intial[j];
+                }
+            }
+            System.out.println(prod);
+            finalarray[i]=prod;
+        }
+
+        for(int i=0;i<a;i++){
+            System.out.print(finalarray[i] + " ");
+        }
 
     }
 }
