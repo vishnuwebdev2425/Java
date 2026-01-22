e8a906ebc6a875ff1e22b7fc40b48ff20341059b
vishnuwebdev2425 <akkupallivishnu2424@gmail.com>
2026-01-22 06:52:43 +0530

    Pair Sum Of 2 Maximum Numbers

--- Practise.java
+++ Practise.java
@@ -87,3 +87,66 @@ class Problem2v2{
         }
     }
 }
+
+class Problem3{
+    static int finaloutput(int[] intialarray){
+        System.out.print("Enter the Number To count : ");
+        Scanner value=new Scanner(System.in);
+        int finalvalue=value.nextInt();
+        int count=0;
+        for (int i=0;i<intialarray.length;i++){
+            if(intialarray[i]==finalvalue){
+                count+=1;
+            }
+
+        }
+        return count;
+
+
+    }
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter the Length Of Number : ");
+        int a =scan.nextInt();
+        int [] intialarray=new int [a];
+        for(int i=0;i<a;i++){
+            System.out.print("Enter the Value That need to add : ");
+            int number=scan.nextInt();
+            intialarray[i]=number;
+        }
+        int finalresult=finaloutput(intialarray);
+        System.out.println("The Count of That Number is : "+ finalresult);
+
+
+
+    }
+}
+
+class Problem4{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter the Length of Number : ");
+        int length=scan.nextInt();
+        int []intialArray=new int [length];
+        for(int i=0;i<length;i++){
+            System.out.print("Enter the Array Values :  ");
+            int addingValue=scan.nextInt();
+            intialArray[i]=addingValue;
+        }
+        int max=0;
+        int SecondMax=0;
+        for(int i=0;i<length;i++){
+            if(intialArray[i] > max){
+                SecondMax=max;
+                max=intialArray[i];
+            }
+            else if(intialArray[i]<max && intialArray[i]>SecondMax){
+                SecondMax=intialArray[i];
+
+            }
+        }
+        int finalresult=max+SecondMax;
+
+        System.out.println("The Sum of Two Largest Number is : " + finalresult + " ");
+    }
+}
